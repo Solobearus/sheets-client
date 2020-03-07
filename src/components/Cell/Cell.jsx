56679839721887116e67bf95e79d53435e3e9ce8
solobearus <ivansolobear@gmail.com>
2020-03-07 22:59:27 +0200

    implement table

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cell.css'
 
 
-const Cell = () => {
-    return (
-        < div className="cell" data-testid="cell">
+const Cell = ({ value, index }) => {
+    const [cellValue, setCellValue] = useState(value);
+    const [editing, setEditing] = useState(false);
 
-        </div >
-    )
+    const onChangeHandle = (event) => {
+        setCellValue(event.target.value);
+    }
+
+    const onClickHandle = () => {
+        setEditing(true);
+    }
+
+    const onBlurHandle = () => {
+        setEditing(false);
+        console.log(index);
+        
+        updateServer();
+    }
+
+    const updateServer = () => {
+        index.text = cellValue;
+
+        fetch('http://localhost:3000/api/Sheet/Save',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    index
+                )
+            })
+            .then()
+            .catch();
+    }
+    return editing ?
+        <input
+            type="text"
+            className="cell"
+            onChange={onChangeHandle}
+            value={cellValue}
+            onBlur={() => onBlurHandle()}
+            autoFocus />
+        :
+        <div
+            className="cell"
+            onClick={() => onClickHandle()}
+        >{cellValue}</div>
 }
 
 export default Cell
--- src/components/Sheet/Sheet.jsx
+++ src/components/Sheet/Sheet.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Sheet.css'
+import Cell from '../Cell/Cell';
 
 
-const Sheet = () => {
+//TODO:fix abc
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWZ'.toLowerCase().split('')
+
+const Sheet = ({ rows, cols }) => {
+
+    const [cells, setCells] = useState({});
+
+    const mock = {
+        "Cells":
+        {
+            "1": {
+                "a": "aaaaaa1 aaaaaa1 aaaaaa1 aaaaa1",
+                "b": "bbbbbb1",
+            },
+            "2": {
+                "a": "aaaaaa12",
+                "b": "bbbbbb12",
+                "c": "ccccccc2",
+            }
+        }
+    }
+
+    useEffect(() => {
+        setCells(mock.Cells);
+    }, [])
+
+    const getCellValue = (i, j) => {
+        return cells[j] && cells[j][ALPHABET[i]] ? cells[j][ALPHABET[i]] : [ALPHABET[i]] + j;
+    }
+
+    const getRow = (row) => {
+        return (new Array(cols))
+            .fill(0)
+            .map((value, col) => (<Cell key={col} value={getCellValue(row, col)} index={{ col: col, row: ALPHABET[row] }}></Cell>))
+    }
+
     return (
-        < div className="sheet" data-testid="sheet">
-
-        </div >
+        <div className="sheet" data-testid="sheet">
+            {
+                (new Array(rows)).fill(0).map((column, row) => {
+                    return (<div className="column" key={row}>{getRow(row)}</div>)
+                })
+            }
+        </div>
     )
 }
 
